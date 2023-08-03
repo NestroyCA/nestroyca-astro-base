c86605852f5ab34af13f30eca76b851a4c67bf48
cfhaak <c.f.haak@posteo.de>
2023-08-03 10:49:15 +0200

    added base to navlinks

--- src/config/navigation.config.ts
+++ src/config/navigation.config.ts
@@ -5,43 +5,44 @@ import {
 	word_graph,
 } from "@/config/image_metadata.config";
 
+const base = "/nestroyca-astro-base";
 export const links = {
 	home: {
-		href: "/",
+		href: base.concat("/"),
 		label: "Home",
 		title: "Home",
 	},
 	nestroylang: {
-		href: "/nestroylang",
+		href: base.concat("/nestroylang"),
 		label: "Nestroy’s Language",
 		title: "Language Structure and Use in Nestroy’s Plays",
 		image: word_graph,
 	},
 	corpus_download: {
-		href: "/nestroylang",
+		href: base.concat("/nestroylang"),
 		label: "Download Corpus",
 		title: "Teaser: TEI annotated theatrical corpus according to the dracor.org schema.",
 		image: corpus,
 	},
 	explore_corpus: {
-		href: "/nestroylang",
+		href: base.concat("/nestroylang"),
 		label: "Explore the digital corpus",
 		title: "Explore the digital corpus (under construction)",
 		image: explore_corpus,
 	},
 	projectvienna: {
-		href: "/nestroyvienna",
+		href: base.concat("/nestroyvienna"),
 		label: "Nestroy’s Vienna",
 		title: "Nestroy’s Vienna",
 		image: painting_vienna,
 	},
 	entities: {
-		href: "/entities",
+		href: base.concat("/entities"),
 		label: "Entities",
 		title: "Entities",
 	},
 	team: {
-		href: "/team",
+		href: base.concat("/team"),
 		label: "Team",
 		title: "Team",
 	},
