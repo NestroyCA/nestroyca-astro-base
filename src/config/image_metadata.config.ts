cabfaaa74966fa6afe97f14648480fbecc059033
cfhaak <c.f.haak@posteo.de>
2023-08-10 15:18:43 +0200

    removed dummy img, added metadata for new img, closes #4

--- src/config/image_metadata.config.ts
+++ src/config/image_metadata.config.ts
@@ -16,54 +16,53 @@ interface ImageData {
 	source_url?: URL;
 }
 
-const ccby3 = "CCBY 3.0";
 export const ccby3_url = "https://creativecommons.org/licenses/by/3.0/at/deed.de";
 
 export const corpus: ImageData = {
 	title: "",
 	description: "Nestroy Corpus Analysis Project",
 	image: corpus_image,
-	licence: ccby3,
+	licence: "",
 };
 export const logo_vienna: ImageData = {
 	title: "",
 	description: "logo: city of vienna",
 	image: stadt_wien_logo,
-	licence: ccby3,
+	licence: "",
 };
 export const explore_corpus: ImageData = {
 	title: "",
 	description: "magnifying glass ",
 	image: explore_corpus_image,
-	licence: ccby3,
+	licence: "© Ch. Katsikadeli",
 };
 export const logo_jubilaeums_fonds: ImageData = {
 	title: "",
 	description: "logo of Jubiläums Fonds",
 	image: jubilaeums_fonds,
-	licence: ccby3,
+	licence: "",
 };
 export const nestroy_statue: ImageData = {
 	title: "statue of Nestroy",
 	description: "statue of Nestroy",
 	image: nestroy_statue_image,
-	licence: ccby3,
+	licence: "",
 };
 export const painting_vienna: ImageData = {
 	title: "painting of vienna",
 	description: "painting of vienna",
 	image: nestroys_vienna,
-	licence: ccby3,
+	licence: "",
 };
 export const logo_oenb: ImageData = {
 	title: "logo of ÖNB",
 	description: "logo of ÖNB",
 	image: oenb_image,
-	licence: ccby3,
+	licence: "",
 };
 export const word_graph: ImageData = {
 	title: "logo of ÖNB",
 	description: "logo of ÖNB",
 	image: word_graph_image,
-	licence: ccby3,
+	licence: "© Ch. Katsikadeli",
 };
